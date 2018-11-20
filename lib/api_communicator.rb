fda54b2d2a29bc544d7cb43a7880d88a1ae974fa
github username <github email address>
2018-11-20 15:36:28 -0500

    improved

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -2,20 +2,38 @@ require 'rest-client'
 require 'json'
 require 'pry'
 
+
+def get_and_parse_data(url)
+  response_string = RestClient.get(url)
+  response_hash = JSON.parse(response_string)
+end
+
+def get_character(character_name)
+  character_array = []
+  response_hash = get_and_parse_data("https://swapi.co/api/people/")
+  is_end = false
+  until is_end
+    next_page = response_hash['next']
+    character_array.concat(response_hash['results'])
+    if response_hash['next'] == nil
+      is_end = true
+    else
+      response_hash = get_and_parse_data(next_page)
+    end
+  end
+  character_array.find do |person_info|
+    person_info['name'] ==character_name
+  end
+end
+
 def get_character_movies_from_api(character_name)
 
-  #make the web request
-  response_string = RestClient.get('http://www.swapi.co/api/people/')
-  response_hash = JSON.parse(response_string)
+  character_hash = get_character(character_name)
 
-  character_hash = response_hash['results'].find do |person_info|
-    person_info['name'] == character_name
-  end
   films = character_hash['films']
 
   films.map do |film|
-    film_info = RestClient.get("#{film}")
-    parsed_response = JSON.parse(film_info)
+    film_info = get_and_parse_data(film)
   end
   # iterate over the response hash to find the collection of `films` for the given
   #   `character`
@@ -33,9 +51,6 @@ def print_movies(films)
     puts "#{i+1} #{film['title']}"
   end
   # some iteration magic and puts out the movies in a nice list
-  films.each_with_index do |film, i|
-    puts "#{i + 1} #{film["title"]}"
-  end
 end
 
 def show_character_movies(character)
