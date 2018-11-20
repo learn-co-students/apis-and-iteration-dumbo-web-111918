30dd91613b70011422e215b1e8bbabf6727268f6
Flatiron School <flatironschool@Flatirons-MacBook-Air-4.local>
2018-11-20 11:58:34 -0500

    Adds welcome and gets character

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,8 +1,11 @@
 def welcome
-  # puts out a welcome message here!
+  put "Welcome to Star Wars Finder!"
 end
 
 def get_character_from_user
   puts "please enter a character name"
-  # use gets to capture the user's input. This method should return that input, downcased.
+  input = gets.chomp
 end
+
+
+get_character_from_user
