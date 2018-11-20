203070020e6259eb2920eb59343a4067c9357f2e
github username <github email address>
2018-11-20 15:55:05 -0500

    added dope intros

--- bin/run.rb
+++ bin/run.rb
@@ -5,5 +5,4 @@ require_relative "../lib/api_communicator.rb"
 require_relative "../lib/command_line_interface.rb"
 
 welcome
-character = get_character_from_user
-show_character_movies(character)
+ask_user_for_mov_or_char
--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -2,16 +2,38 @@ require 'rest-client'
 require 'json'
 require 'pry'
 
-def get_character_movies_from_api(character_name)
-  #make the web request
+def url_stuff
   response_string = RestClient.get('http://www.swapi.co/api/people/')
   response_hash = JSON.parse(response_string)
+  #binding.pry
+end
+#-------------------------------------------------------------------------------
+# GET CHARACTERS FROM API
+#-------------------------------------------------------------------------------
+def get_characters()
+  response_string = RestClient.get('http://www.swapi.co/api/people/')
+  response_hash = JSON.parse(response_string)
+end
+#-------------------------------------------------------------------------------
+# GET MOVIES FROM by SEARCHING CHARACTER NAME API
+#-------------------------------------------------------------------------------
 
-  characters = response_hash['results']
+# def get_characters(name)
+#   response_string = RestClient.get("http://www.swapi.co/api/people/?search=#{name}")
+#   response_hash = JSON.parse(response_string)
+#   # binding.pry
+# end
 
+#-------------------------------------------------------------------------------
+# GET MOVIES FROM CHARACTERS
+#-------------------------------------------------------------------------------
+def get_character_movies_from_api(character_name)
+
+  characters = get_characters()['results']
+  # binding.pry
   films = []
   characters.each do |character|
-    if character['name'] == character_name
+    if character['name'].upcase.include?(character_name.upcase)
       films = character["films"]
     end
   end
@@ -25,7 +47,7 @@ def get_character_movies_from_api(character_name)
 
 end
 
-
+# binding.pry
 
 def print_movies(films)
   film_names = []
@@ -46,7 +68,47 @@ def show_character_movies(character)
 end
 
 # binding.pry
-## BONUS
+## BONUS ------------------------------------------------------------------------\
+
+def show_movie_crawl
+  puts "please enter a movie"
+  ans = gets.chomp
+  ans
+end
+
+def get_movies()
+  response_string = RestClient.get('http://www.swapi.co/api/films/')
+  response_hash = JSON.parse(response_string)
+end
+
+def get_movie_crawl_from_api(movie_name)
+
+  movies = get_movies['results']
+  starwars = nil
+  movies.map do |movie|
+    if movie['title'].upcase.include?(movie_name.upcase)
+      starwars = movie['opening_crawl']
+      # binding.pry
+    end
+    starwars
+  end
+  starwars
+  # binding.pry
+end
+
+# binding.pry
+
+def show_movie_crawl(movie)
+  crawl = get_movie_crawl_from_api(movie)
+  # binding.pry
+  if crawl == nil
+    puts "I bet you like sand"
+  else
+    puts crawl
+  end
+end
+
+
 
 # that `get_character_movies_from_api` method is probably pretty long. Does it do more than one job?
 # can you split it up into helper methods?
